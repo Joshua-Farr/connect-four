c9741705783067d1614d389fe676e391b052d1d5
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-10-14 20:13:22 -0500

    Closer to getting it to render correctly.

--- src/components/GameBoard/Gameboard.tsx
+++ src/components/GameBoard/Gameboard.tsx
@@ -1,3 +1,4 @@
+import { render } from "react-dom";
 import GamePiece from "../GamePiece/GamePiece";
 import "./GameBoard.css";
 
@@ -19,19 +20,34 @@ export default function GameBoard(gameboard: GameBoardProps) {
   function createBoard() {
     let renderedBoard: any = []; // need to fix the TypeScript on this...
 
-    for (let i = 0; i < fakeGame.length; i++) {
-      console.log(fakeGame[i]);
-      const column = [];
-      for (let j = 0; j < 6; j++) {
+    // for (let i = 0; i < fakeGame.length; i++) {
+    //   console.log(fakeGame[i]);
+    //   const column = [];
+    //   for (let j = 0; j < 6; j++) {
+    //     if (fakeGame[i][j]) {
+    //       column.push(<GamePiece playerNumber={fakeGame[i][j]} />);
+    //     } else if (!fakeGame[i][j]) {
+    //       column.push(<GamePiece playerNumber={3} />);
+    //     }
+    //   }
+    //   renderedBoard.push(column);
+    // }
+
+    for (let j = 6; j > 0; j--) {
+      // let row = [];
+      for (let i = 0; i < 7; i++) {
         if (fakeGame[i][j]) {
-          column.push(<GamePiece playerNumber={fakeGame[i][j]} />);
-        } else if (!fakeGame[i][j]) {
-          column.push(<GamePiece playerNumber={3} />);
+          console.log(`Generating piece for spot for Column: ${i} Row: ${j}`);
+          renderedBoard.push(<GamePiece playerNumber={fakeGame[i][j]} />);
+        } else {
+          console.log(`Generating piece for spot for Column: ${i} Row: ${j}`);
+          renderedBoard.push(<GamePiece playerNumber={3} />);
         }
       }
-      renderedBoard.push(column);
+      // renderedBoard.push(row);
     }
 
+    console.log(renderedBoard);
     return renderedBoard;
   }
 
