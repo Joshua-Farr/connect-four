57da9026aaaa575fe8cce735ce13c6bb22190ab8
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-10-19 18:45:25 -0500

    half the board diagonal logic completed

--- src/ConnectFour.tsx
+++ src/ConnectFour.tsx
@@ -18,10 +18,11 @@ export default class ConnectFour {
   }
 
   placePiece(rowNumber: number) {
-    if (rowNumber > this.width) {
-      throw new Error("Cannot play a piece outside of the board!");
-    } else if (this.checkForWin()) {
+    if (this.gameOver) {
       throw new Error("Game has already concluded!");
+    }
+    if (rowNumber >= this.width) {
+      throw new Error("Cannot play a piece outside of the board!");
     } else if (this.board[rowNumber].length === this.height) {
       console.log("This column is already full!");
       return;
@@ -29,54 +30,123 @@ export default class ConnectFour {
 
     this.board[rowNumber].push(this.player);
 
-    this.player === 1 ? (this.player = 2) : (this.player = 1);
+    if (this.checkForWin()) {
+      console.log(`${this.player} is the winner!!`);
+      this.gameOver = true;
+    } else {
+      this.player === 1 ? (this.player = 2) : (this.player = 1);
+    }
   }
 
   checkForWin() {
     console.log("CHECKING FOR THE WIN...");
     // console.log("Here is the board that we're checking: ", this.board);
 
-    //Checking verticals
-    for (let i = 0; i < this.board.length; i++) {
-      let count = 0;
-      for (let j = 0; j < this.board[i].length; j++) {
-        if (this.board[i][j] === this.player) {
-          count++;
-          console.log(
-            `THE VERT COUNT IS: ${count} for player #${this.player}, count is starting at ${i}x${j}`
-          );
-        } else {
-          count = 0;
-        }
-        if (count === 4) {
-          console.log("GAME OVER!");
-          console.log(`PLAYER ${this.player} IS THE WINNER!`);
-          return true;
-        }
-      }
-    }
+    // Checking verticals
+    // for (let i = 0; i < this.board.length; i++) {
+    //   let count = 0;
+    //   for (let j = 0; j < this.board[i].length; j++) {
+    //     if (this.board[i][j] === this.player) {
+    //       count++;
+    //       console.log(
+    //         `THE VERT COUNT IS: ${count} for player #${this.player}, count is starting at ${i}x${j}`
+    //       );
+    //     } else {
+    //       count = 0;
+    //     }
+    //     if (count === 4) {
+    //       console.log("GAME OVER!");
+    //       console.log(`PLAYER ${this.player} IS THE WINNER!`);
+    //       this.gameOver = true;
 
-    //Check Horizontals
+    //       return true;
+    //     }
+    //   }
+    // }
 
-    for (let i = 0; i < this.height; i++) {
-      let count = 0;
-      for (let j = 0; j < this.board.length; j++) {
-        if (this.board[j][i] === this.player) {
-          count++;
-          console.log(`THE HOR COUNT IS: ${count} for ${this.player}`);
-        } else {
-          count = 0;
-        }
-        if (count === 4) {
-          console.log("GAME OVER!");
-          console.log(`PLAYER ${this.player} IS THE WINNER!`);
-          return true;
-        }
-      }
-    }
+    // // Check Horizontals
+
+    // for (let i = 0; i < this.height; i++) {
+    //   let count = 0;
+    //   for (let j = 0; j < this.board.length; j++) {
+    //     if (this.board[j][i] === this.player) {
+    //       count++;
+    //       console.log(
+    //         `THE HOR COUNT IS: ${count} for player #${this.player}, count is starting at ${i}x${j}`
+    //       );
+    //     } else {
+    //       count = 0;
+    //     }
+    //     if (count === 4) {
+    //       console.log("GAME OVER!");
+    //       console.log(`PLAYER ${this.player} IS THE WINNER!`);
+    //       this.gameOver = true;
+
+    //       return true;
+    //     }
+    //   }
+    // }
 
     //Check Diagonals
 
-    return false;
+    //Checking main diagonals
+
+    // if (
+    //   this.board[0][3] === this.player &&
+    //   this.board[1][2] === this.player &&
+    //   this.board[2][1] === this.player &&
+    //   this.board[3][0] === this.player
+    // ) {
+    //   console.log("GAME OVER!");
+    //   console.log(`PLAYER ${this.player} IS THE WINNER!`);
+    //   this.gameOver = true;
+    //   return true;
+    // }
+
+    // if (
+    //   this.board[3][this.height - 1] === this.player &&
+    //   this.board[4][this.height - 2] === this.player &&
+    //   this.board[5][this.height - 3] === this.player &&
+    //   this.board[6][this.height - 4] === this.player
+    // ) {
+    //   console.log("GAME OVER!");
+    //   console.log(`PLAYER ${this.player} IS THE WINNER!`);
+    //   this.gameOver = true;
+    //   return true;
+    // }
+
+    let count = 0;
+    // for (let x = 0; x < this.width; x++) {
+    //   if (this.board[x][this.height - 1 - x] === this.player) {
+    //     count++;
+    //   } else {
+    //     count = 0;
+    //   }
+    //   if (count === 4) {
+    //     console.log("GAME OVER!");
+    //     console.log(`PLAYER ${this.player} IS THE WINNER!`);
+    //     this.gameOver = true;
+
+    //     return true;
+    //   }
+    // }
+
+    for (let i = 0; i < this.width; i++) {
+      count = 0;
+      for (let x = 0; x < this.width; x++) {
+        if (this.board[x][this.height - 1 - i - x] === this.player) {
+          count++;
+        } else {
+          count = 0;
+        }
+        if (count === 4) {
+          console.log("GAME OVER!");
+          console.log(`PLAYER ${this.player} IS THE WINNER!`);
+          this.gameOver = true;
+
+          return true;
+        }
+      }
+    }
   }
 }
